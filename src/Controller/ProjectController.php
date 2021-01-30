f3ed2fe18e3dc87ecb201ae37cd19269a1c7291c
Jurjen Vos <jurjen.vos@webstores.nl>
2021-01-30 20:03:53 +0100

    feat: added static project detail template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -8,18 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProjectController extends AbstractController
 {
     /**
-     * @Route("/projects", name="app_projects")
+     * @Route("/projects", name="app_project_overview")
      */
     public function projects()
     {
-        return $this->render('pages/projects-overview.html.twig');
+        return $this->render('pages/project/project-overview.html.twig');
     }
 
     /**
-     * @Route("/projects/project", name="app_project")
+     * @Route("/projects/{slug}", name="app_project_detail")
      */
-    public function project()
+    public function project(string $slug)
     {
-        return $this->render('pages/project-detail.html.twig');
+        return $this->render('pages/project/project-detail.html.twig', [
+            'project' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 }
