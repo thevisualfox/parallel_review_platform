8756f1537eb86b4ce177b74aa062387edcfc52a5
Jurjen Vos <jurjen.vos@webstores.nl>
2021-01-31 16:34:11 +0100

    feat (login): add register functionality

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
